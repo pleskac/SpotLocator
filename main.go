cdb06bf4acbcc6044e6efdc64c56dbba4eb7fccb
Mark Pleskac <markpleskac@gmail.com>
2012-08-22 20:45:00 -0500

    Add tweet parsing

--- main.go
+++ main.go
@@ -2,24 +2,30 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
 )
 
 func main() {
-	for{
+	for {
 		fmt.Println("Pulling tweets... ")
 		list, err := GetNewTweets()
 
-		if err != nil{
+		if err != nil {
 			continue
 		}
-		
-		for _, tweet := range *list{
-			fmt.Println(tweet)
+
+		for _, tweet := range *list {
+			if strings.HasPrefix(tweet.Text, "Start") {
+
+			} else if strings.HasPrefix(tweet.Text, "End") {
+
+			} else if strings.HasPrefix((tweet.Text), "http://t.co/") {
+				fmt.Println("NEW LOCATION:", tweet)
+			}
 		}
-		
 
 		//SaveTweets(newTweets)
-		time.Sleep(10000  * time.Millisecond)
+		time.Sleep(10000 * time.Millisecond)
 	}
 }
