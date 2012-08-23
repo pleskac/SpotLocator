2c808101af0fb93770f800d7458a9cfd3ff4d4ce
Mark Pleskac <markpleskac@gmail.com>
2012-08-22 23:31:27 -0500

    work on parsing Spot JSON

--- mysql.go
+++ mysql.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
+	"github.com/ziutek/mymysql/mysql"
+	_ "github.com/ziutek/mymysql/native" // Native engine
+	// _ "github.com/ziutek/mymysql/thrsafe" // Thread safe engine
 )
 
 func AddGPS(longitude float32, latitude float32) {
 	fmt.Println("NEW LOCATION:", longitude, latitude)
+
+	db := mysql.New("tcp", "", "127.0.0.1:3306", "root", "rootroot", gps)
 
 	//NEED TO IMPLEMENT THIS!!
 
