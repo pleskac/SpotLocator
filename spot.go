e06f3c71f348fb39fd7cb48fd98faec67b70fb72
Mark Pleskac <markpleskac@gmail.com>
2012-11-10 10:16:12 -0600

    Stopped calling Twitter. Using SPOT API now. Parsed the JSON, no functionality fully working

--- spot.go
+++ spot.go
@@ -1,34 +1,112 @@
 package main
 
 import (
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
-type WTFISTHIS struct {
-	Response Response
+type SpotApiResponse struct {
+	Resp Response `json:"response"`
 }
 
 type Response struct {
-	MessagesResponse MessagesResponse
+	FeedMsgResp FeedMessageResponse `json:"feedMessageResponse"`
 }
 
-type MessagesResponse struct {
-	Messages Messages
+type FeedMessageResponse struct {
+	Count         int      `json:"count"`
+	Feed          Feed     `json:"feed"`
+	TotalCount    int      `json:"totalCount"`
+	ActivityCount int      `json:"activityCount"`
+	Messages      Messages `json:"messages"` // this ought to be an array but it's not from Spot? this might break...
+}
+
+type Feed struct {
+	Id                   string `json:"id"`
+	Name                 string `json:"name"`
+	Description          string `json:"description"`
+	Status               string `json:"status"`
+	Usage                int    `json:"usage"`
+	DaysRange            int    `json:"daysRange"`
+	DetailedMessageShown bool   `json:"detailedMessageShown"`
 }
 
 type Messages struct {
-	Message Message
+	Message []Message `json:"message"`
 }
 
 type Message struct {
-	Latitude       float32
-	Longitude      float32
-	MessageContent string
-	Time           int64
+	AtClientUnixTime string  `json:"@clientUnixTime"`
+	Id               int     `json:"id"`
+	MessengerId      string  `json:"messengerId"`
+	MessengerName    string  `json:"messengerName"`
+	UnixTime         int     `json:"unixTime"`
+	MessageType      string  `json:"messageType"`
+	Latitude         float64 `json:"latitude"`
+	Longitude        float64 `json:"longitude"`
+	ShowCustomMsg    string  `json:"showCustomMsg"`
+	DateTime         string  `json:"dateTime"`
+	MessageDetail    string  `json:"messageDetail"`
+	Selected         bool    `json:"selected"`
+	Altitude         int     `json:"altitude"`
+	Hidden           int     `json:"hidden"`
+	MessageContent   string  `json:"messageContent"`
 }
 
+type NewLocations []Message
+
+func getJsonObject(feedId string) (*SpotApiResponse, error) {
+	url := "https://api.findmespot.com/spot-main-web/consumer/rest-api/2.0/public/feed/" + feedId + "/message.json"
+
+	fmt.Println("URL:", url)
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+	resp, err := client.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	dec := json.NewDecoder(resp.Body)
+	defer resp.Body.Close()
+
+	spotResp := &SpotApiResponse{}
+	if err := dec.Decode(spotResp); err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	fmt.Println(spotResp)
+
+	return spotResp, nil
+}
+
+func GetNewLocations(feedId string, id int) (*NewLocations, error) {
+	json, err := getJsonObject(feedId)
+
+	if err != nil {
+		return nil, err
+	}
+	//FILTER
+
+	for _, mes := range json.Resp.FeedMsgResp.Messages.Message {
+		fmt.Println(mes.Id)
+	}
+
+	/*for mes := range jsonStructure.Response.FeedMessageResponse.Messages.Message {
+		fmt.Println(mes)
+	}
+	*/
+	//jsonStructure.Response.MessagesResponse.Messages
+	return nil, nil
+}
+
+/*
 func GetGPSLocationFromId(id string) (float32, float32, string, int64, error) {
 	url := "http://share.findmespot.com/spot-adventures/rest-api/1.0/public/location" + id
 	fmt.Println(url)
@@ -42,7 +120,7 @@ func GetGPSLocationFromId(id string) (float32, float32, string, int64, error) {
 	defer resp.Body.Close()
 
 	dec := json.NewDecoder(resp.Body)
-	res := new(WTFISTHIS)
+	res := new(TopStructure)
 
 	if err = dec.Decode(res); err != nil {
 		fmt.Println("Error decoding:", err)
@@ -58,3 +136,4 @@ func GetGPSLocationFromId(id string) (float32, float32, string, int64, error) {
 	fmt.Println("Timestamp:", tm)
 	return long, lat, msg, tm, nil
 }
+*/
