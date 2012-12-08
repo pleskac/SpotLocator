dad9262b530e5bc04690f66f25e348077204b552
Mark Pleskac <markpleskac@gmail.com>
2012-12-08 12:27:43 -0600

    Added Gorilla/Mux. Also added different JSON routing. Legacy should work, new stuff should print and not work

--- endpoint.go
+++ endpoint.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"code.google.com/p/gorilla/mux"
 	"encoding/json"
-	"log"
+	"fmt"
+	_ "log"
 	"net/http"
 )
+
+const tripId = "tripId"
 
 func tripOutput(w http.ResponseWriter, r *http.Request) {
 	output := GetCurrentTrip()
@@ -14,9 +18,24 @@ func tripOutput(w http.ResponseWriter, r *http.Request) {
 }
 
 func endpoint() {
-	//http://localhost:8080/trip.json
-	http.HandleFunc("/trip.json", tripOutput)
+	/*
+		//http://localhost:8080/trip.json
+		http.HandleFunc("/trip.json", tripOutput)
 
-	//Blocks and runs in a loop
-	log.Fatal(http.ListenAndServe(":8080", nil))
+		//Blocks and runs in a loop
+		log.Fatal(http.ListenAndServe(":8080", nil))
+	*/
+	r := mux.NewRouter()
+	r.HandleFunc("/trips/{"+tripId+"}", TripHandler)
+	r.HandleFunc("/currentTrip", tripOutput)
+	//legacy. remove later.
+	r.HandleFunc("/trip.json", tripOutput)
+	//can add other JSON handlers here
+	http.Handle("/", r)
+}
+
+func TripHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars[tripId]
+	fmt.Println("Id:", id)
 }
