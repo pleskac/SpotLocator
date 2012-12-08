d01ca85c6ea546e2f4214eafb94971e790f7ff84
Mark Pleskac <markpleskac@gmail.com>
2012-12-08 12:38:40 -0600

    Added subrouter for 8080

--- endpoint.go
+++ endpoint.go
@@ -25,13 +25,14 @@ func endpoint() {
 		//Blocks and runs in a loop
 		log.Fatal(http.ListenAndServe(":8080", nil))
 	*/
-	r := mux.NewRouter()
+	router := mux.NewRouter()
+	r := router.Host("{domain:pleskac.org:8080|localhost}").Subrouter()
 	r.HandleFunc("/api/trips/{"+tripId+"}", TripHandler)
 	r.HandleFunc("/api/currentTrip", tripOutput)
 	//legacy. remove later.
 	r.HandleFunc("/trip.json", tripOutput)
 	//can add other JSON handlers here
-	http.Handle(":8080/", r)
+	http.Handle("/", r)
 }
 
 func TripHandler(w http.ResponseWriter, r *http.Request) {
