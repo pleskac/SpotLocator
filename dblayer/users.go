c7b2109d462fe2fb1729d82188683d916b873462
Mark Pleskac <markpleskac@gmail.com>
2013-07-08 18:21:02 -0500

    added a function to access all users. start of the transition to make this accessable to the public

---
+++ dblayer/users.go
@@ -0,0 +1,26 @@
+package dblayer
+
+import (
+	_ "fmt"
+	_ "github.com/ziutek/mymysql/mysql"
+	_ "github.com/ziutek/mymysql/native"
+	_ "time"
+)
+
+func GetAllUsers() []string {
+	var users []string
+	db := Connect()
+	defer db.Close()
+
+	//Get the current trip, if it exists
+	rows, _, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		panic(err)
+	}
+
+	for _, row := range rows {
+		users = append(users, row.Str(0))
+	}
+
+	return users
+}
