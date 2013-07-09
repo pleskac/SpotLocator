7b2c4b58c58d79a2986c1c120e45b53e415e40e2
Mark Pleskac <markpleskac@gmail.com>
2013-07-09 14:52:13 -0500

    not concurrent

--- main.go
+++ main.go
@@ -17,40 +17,6 @@ func main() {
 	go endpoint()
 
 	//LOL this is so bad
-	go newMain()
-	/*
-		//OLD CODE TO KEEP THIS BEAST RUNNING, KEPT FOR SAFE KEEPING
-		//update latestSpotId
-		latestSpotId = dblayer.GetLatestSpotId()
-
-		for {
-			//DO ALL SPOT UPDATES
-			newLocations, err := GetNewLocations("0oCHzmaKo1zRkSHQglD2qqXkT2yJPvzpK", latestSpotId)
-
-			if err != nil {
-				fmt.Println("Error getting new locations:", err)
-			}
-
-			for _, location := range newLocations {
-				fmt.Println("Adding new GPS location", location.MessageType)
-
-				//SPOT returns time in UTC. This will correct the time to the localized time.
-				dblayer.AddGPS_UTC(location.Longitude, location.Latitude, location.MessageContent, location.MessageType, "markpleskac@gmail.com", location.UnixTime)
-
-				if location.Id > latestSpotId {
-					latestSpotId = location.Id
-					dblayer.SaveLatestSpotId(latestSpotId)
-				}
-			}
-
-			//DO OTHER UPDATES FROM OTHER DEVICES 
-
-			//Wait 30 seconds
-			time.Sleep(30000 * time.Millisecond)
-		}*/
-}
-
-func newMain() {
 	//I don't have testing, which I could fix.
 	//I'm also testing in production. 
 	//No-nos that I'm too lazy to change right now.
@@ -93,4 +59,35 @@ func newMain() {
 		time.Sleep(30000 * time.Millisecond)
 
 	} //End of the infinite 'for', wait between users. Bad because the more users, the less refresh rate.
+
+	/*
+		//OLD CODE TO KEEP THIS BEAST RUNNING, KEPT FOR SAFE KEEPING
+		//update latestSpotId
+		latestSpotId = dblayer.GetLatestSpotId()
+
+		for {
+			//DO ALL SPOT UPDATES
+			newLocations, err := GetNewLocations("0oCHzmaKo1zRkSHQglD2qqXkT2yJPvzpK", latestSpotId)
+
+			if err != nil {
+				fmt.Println("Error getting new locations:", err)
+			}
+
+			for _, location := range newLocations {
+				fmt.Println("Adding new GPS location", location.MessageType)
+
+				//SPOT returns time in UTC. This will correct the time to the localized time.
+				dblayer.AddGPS_UTC(location.Longitude, location.Latitude, location.MessageContent, location.MessageType, "markpleskac@gmail.com", location.UnixTime)
+
+				if location.Id > latestSpotId {
+					latestSpotId = location.Id
+					dblayer.SaveLatestSpotId(latestSpotId)
+				}
+			}
+
+			//DO OTHER UPDATES FROM OTHER DEVICES 
+
+			//Wait 30 seconds
+			time.Sleep(30000 * time.Millisecond)
+		}*/
 }
